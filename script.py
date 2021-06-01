45bdeaaba82f21949cb2d15e5738c65282ba8f0a
hashimoto.yit@intern20w <hashimoto.yit@gmail.com>
2021-06-01 11:26:59 +0900

    Webカメラ認識

--- script.py
+++ script.py
@@ -1,8 +1,12 @@
 import cv2
-img = cv2.imread('chrome.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-ret, thresh = cv2.threshold(gray, 127,255, cv2.THRESH_BINARY_INV)
 
-cv2.imshow('OpenCV', thresh)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# 動画再生
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
